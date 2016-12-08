b1704f8df8e12af9eeba897e9fee1c335ca2c2de
zach-is-my-name <zach.greco@gmail.com>
2016-12-08 18:18:13 -0500

    Latest

--- app.js
+++ app.js
@@ -1,17 +1,65 @@
+var player1_score;
+var player2_score;
+var tempurature1;
+var temperature2;
+var clouds1
+var clouds2
+var precip1
+var precip2
 
 var SEARCH_URL = 'api.openweathermap.org/data/2.5/weather?';
 var API_KEY = '9be13ca98f24eafcd8d9c1c5b856347b';
-
-
-// APPID is your unique API key 
+// APPID is your unique API key
 //example API call: http://api.openweathermap.org/data/2.5/weather?q=Atlanta,GA&units=imperial&appid=9be13ca98f24eafcd8d9c1c5b856347b
 
 
-function queryOpenWeather(city, callbackFn) {
-	var params = {
-		q: "Atlanta,GA",
-		units: "imperial",
-		key: API_KEY,
-		}
-	$.getJSON(SEARCH_URL, params, callbackFn);
+// ajax logic
+function queryWeather(city, callbackFn) {
+    var params = {
+        q: searchCity,
+        units: "imperial",
+        key: API_KEY,
+        }
+    $.getJSON(SEARCH_URL, params, function(response){
+      console.log(response);
+    });
 }
+
+//event listening logic
+
+function submission(){
+  $('#js-search').submit(function(event){
+      event.preventDefault();
+      var searchCity = $(event.currentTarget).find('input').val();
+
+      console.log(queryWeather(searchCity, displayWeather()));
+  })
+
+}
+
+//render function
+
+function displayWeather() {
+  var city1temp = main.temp;
+  // console.log(city1temp);
+}
+
+
+// function compare_temp() {
+//   if (tempurature1 > temperature2) {
+//   player1_score +=1
+// }
+// else if (temperature2 > tempurature1) {
+//   player2_score +=1
+//   }
+// }
+//
+// function compare_clouds() {
+//   if ()
+//
+// }
+//
+// function compare_precip () {
+//
+//
+// }
